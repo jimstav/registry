1343ff3a5230845f262d5e52aaaf736e4050d086
Dimitris Stavroulakis <jimstav@gmail.com>
2025-03-14 14:38:52 +0200

    Navigating with a Link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Link to="/">NPM Registry</Link>
+    </div>
+  );
 }
 
 export default Header;
