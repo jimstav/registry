f181e9408e9349f4117c365e62c9602fc90605e3
Dimitris Stavroulakis <jimstav@gmail.com>
2025-03-15 18:01:23 +0200

    Accessing the Data from a Loader

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,11 @@
+import { useLoaderData } from "react-router-dom";
+import { HomeLoaderResult } from "./homeLoader";
+
 function HomePage() {
+  const { featuredPackages } = useLoaderData() as HomeLoaderResult;
+
+  console.log(featuredPackages);
+
   return <div>HomePage</div>;
 }
 
